083cbced8623059f1306de84cf1176a4a62b8348
Najee Burnette <najeeburnette@gmail.com>
2021-12-07 12:55:18 -0800

    initial commit

---
+++ src/Main/Main.java
@@ -0,0 +1,21 @@
+package Main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource(fxml file));
+        stage.setTitle("First Screen");
+        stage.setScene(new Scene(root, 800, 600));
+        stage.show();
+    }
+
+    public static void main(String[] args){
+        launch(args);
+    }
+}
