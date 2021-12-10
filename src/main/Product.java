a812f06b571fb34a5999946b13ced8fefa28e2f7
Najee Burnette <najeeburnette@gmail.com>
2021-12-09 17:18:29 -0800

    Updated FXML files, added InHouse, Outsourced, Part, Product classes

---
+++ src/main/Product.java
@@ -0,0 +1,82 @@
+package main;
+
+
+import javafx.collections.ObservableList;
+
+public class Product {
+    private ObservableList associatedParts;
+    private int id;
+    private String name;
+    private double price;
+    private int stock;
+    private int min;
+    private int max;
+    public Product(int id, String name, double price, int stock,int min, int max){
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+        this.min = min;
+        this.max = max;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setMin(int min) {
+        this.min = min;
+    }
+
+    public int getMin() {
+        return min;
+    }
+
+    public void setMax(int max) {
+        this.max = max;
+    }
+
+    public int getMax() {
+        return max;
+    }
+
+    public void addAssociatedPart(Part part){
+
+    }
+
+    public boolean deleteAssociatedPart(Part selectAssociatedPart){
+
+    }
+
+    public ObservableList getAllAssociatedParts(){
+
+    }
+}
